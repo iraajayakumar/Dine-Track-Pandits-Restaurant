21d5e9e950be45673f8e61a875d529143b4b34a1
Iraa Jayakumar <156112359+iraajayakumar@users.noreply.github.com>
2025-04-24 19:13:37 +0530

    Added Modal Pop Up forms to Add Data

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Add Button Model Popup
+const modal = document.getElementById("modalOverlay");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementById("closeModalBtn");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
